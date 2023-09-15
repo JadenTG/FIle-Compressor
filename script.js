f391635e3f8808bc1bdaca8b6b8e2b8a8e66038d
JadenTG <111470273+JadenTG@users.noreply.github.com>
2023-09-15 14:01:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const multer = require('multer')
+const fs = require('fs');
+const zopfli = require('node-zopfli');
+
+const app = express();
+const upload = multer();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('./views'));
+app.use(express.static('./public'));
+
+app.get('/', (req,res) => {
+    res.render("upload");
+});
+
+const storage = multer.diskStorage({
+    des: './views/upload',
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+app.post('/smash', upload.single('blank'), function(req,res) {  
+
+    const file = req.file;
+
+    const options = {
+        verbose: false,
+        verbose_more: false,
+        numiterations: 15,
+        blocksplitting: true,
+        blocksplittinglast: false,
+        blocksplittingmax: 15
+    }
+
+    fs.createReadStream(file.originalname)
+    .pipe(zopfli.createGzip(options))
+    .pipe(fs.createWriteStream(`${file.originalname}.gz`));
+
+    res.redirect('/');
+})
+
+
+app.listen(port, (req,res) => {
+    console.log('Server started', port)
+});
